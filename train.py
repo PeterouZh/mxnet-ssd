adfc4fdc41cbafbeba4409cb8a039851d6d0ee31
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2017-02-27 17:11:21 +0800

    Md train.py

--- train.py
+++ train.py
@@ -23,10 +23,10 @@ def parse_args():
                         choices=['vgg16_reduced'], help='which network to use')
     parser.add_argument('--batch-size', dest='batch_size', type=int, default=32,
                         help='training batch size')
-    parser.add_argument('--resume', dest='resume', type=int, default=-1,
-                        help='resume training from epoch n')
-    parser.add_argument('--finetune', dest='finetune', type=int, default=-1,
-                        help='finetune from epoch n, rename the model before doing this')
+    parser.add_argument('--resume', dest='resume', type=int,
+                        default=-1, help='resume training from epoch n')
+    parser.add_argument('--finetune', dest='finetune', type=int,
+                        default=-1, help='finetune from epoch n, rename the model before doing this')
     parser.add_argument('--pretrained', dest='pretrained', help='pretrained model prefix',
                         default=os.path.join(os.getcwd(), 'model', 'vgg16_reduced'), type=str)
     parser.add_argument('--epoch', dest='epoch', help='epoch of pretrained model',
@@ -38,31 +38,31 @@ def parse_args():
     parser.add_argument('--begin-epoch', dest='begin_epoch', help='begin epoch of training',
                         default=0, type=int)
     parser.add_argument('--end-epoch', dest='end_epoch', help='end epoch of training',
-                        default=200, type=int)
+                        default=1000, type=int)
     parser.add_argument('--frequent', dest='frequent', help='frequency of logging',
                         default=20, type=int)
     parser.add_argument('--data-shape', dest='data_shape', type=int, default=300,
                         help='set image shape')
-    parser.add_argument('--lr', dest='learning_rate', type=float, default=0.002,
-                        help='learning rate')
-    parser.add_argument('--momentum', dest='momentum', type=float, default=0.9,
-                        help='momentum')
-    parser.add_argument('--wd', dest='weight_decay', type=float, default=0.0001,
-                        help='weight decay')
+    parser.add_argument('--lr', dest='learning_rate', type=float,
+                        default=0.001, help='learning rate')
+    parser.add_argument('--momentum', dest='momentum', type=float,
+                        default=0.9, help='momentum')
+    parser.add_argument('--wd', dest='weight_decay', type=float,
+                        default=0.0001, help='weight decay')
     parser.add_argument('--mean-r', dest='mean_r', type=float, default=123,
                         help='red mean value')
     parser.add_argument('--mean-g', dest='mean_g', type=float, default=117,
                         help='green mean value')
     parser.add_argument('--mean-b', dest='mean_b', type=float, default=104,
                         help='blue mean value')
-    parser.add_argument('--lr-epoch', dest='lr_refactor_epoch', type=int, default=20,
-                        help='refactor learning rate every N epoch')
-    parser.add_argument('--lr-ratio', dest='lr_refactor_ratio', type=float, default=0.8,
-                        help='ratio to refactor learning rate')
-    parser.add_argument('--log', dest='log_file', type=str, default="train.log",
-                        help='save training log to file')
-    parser.add_argument('--monitor', dest='monitor', type=int, default=0,
-                        help='log network parameters every N iters if larger than 0')
+    parser.add_argument('--lr-epoch', dest='lr_refactor_epoch', type=int,
+                        default=1000000, help='refactor learning rate every N epoch')
+    parser.add_argument('--lr-ratio', dest='lr_refactor_ratio', type=float,
+                        default=0.5, help='ratio to refactor learning rate')
+    parser.add_argument('--log', dest='log_file', type=str,
+                        default="train.log", help='save training log to file')
+    parser.add_argument('--monitor', dest='monitor', type=int,
+                        default=0, help='log network parameters every N iters if larger than 0')
     args = parser.parse_args()
     return args
 
@@ -72,7 +72,7 @@ if __name__ == '__main__':
     ctx = mx.cpu() if not ctx else ctx
     # resume
     args.prefix = os.path.join(os.getcwd(), 'model', 'ssd')
-    args.resume = 199
+    args.resume = 257
     train_net(args.network, args.dataset, args.image_set, args.year,
               args.devkit_path, args.batch_size,
               args.data_shape, (args.mean_r, args.mean_g, args.mean_b),
